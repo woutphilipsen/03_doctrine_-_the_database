947f8d836d88abdd956c24cbdcc06221969afea1
Wout Philipsen <Wout.Philipsen@ad.core.vdabantwerpen.intra>
2019-12-11 13:20:16 +0100

    first commit

---
+++ src/Controller/ArticleAdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleAdminController extends AbstractController {
+
+    /**
+     * @Route("/admin/article/new")
+     */
+    public function new()
+    {
+        return new Response('space rocks..');
+    }
+
+}
